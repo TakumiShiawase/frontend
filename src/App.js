14144ee9ad3e137681a844e7a7c65443fc15eead
Tima <takumishiawase.it@gmail.com>
2023-08-31 03:54:57 +0600

    rist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = { details: [],}
+
+  componentDidMount(){
+    let data;
+    axios.get('http://localhost:8000')
+    .then(res => {
+      data = res.data;
+      this.setState({
+        details: data
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <header>Наши главы</header>
+          <hr></hr>
+          {this.state.details.map((output, id) => (
+            <div key={id}>
+              <div>
+                <h2>{output.title}</h2>
+                <p>{output.content}</p>
+              </div>
+            </div>
+          ))}
+        
+      </div>
+    )
+  }
 }
 
+
 export default App;
+
